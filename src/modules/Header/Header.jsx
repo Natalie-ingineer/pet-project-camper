675e20bf2cef0a1c8156abd0d1d0e98e9aa1556e
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-06-05 19:21:17 +0300

    add icons

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,11 +1,24 @@
+import { sprite } from 'shared/icons';
 import s from './Header.module.scss';
 
 const Header = () => {
   return (
     <div>
-      <svg className={s.svg} width="20" height="20">
-        <use href="#users"></use>
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-star`}></use>
       </svg>
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-close`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-kitchen`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-petrol`}></use>
+      </svg>
+
       <h1>Home Page</h1>
     </div>
   );
