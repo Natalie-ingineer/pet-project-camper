65b9c7824fa094b8aaa1c3af6aed65d9803e2f81
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-06-05 20:24:30 +0300

    add icons last

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -19,6 +19,82 @@ const Header = () => {
         <use xlinkHref={`${sprite}#icon-petrol`}></use>
       </svg>
 
+      <svg width="24" height="24">
+        <use xlinkHref={`${sprite}#icon-air`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-calendar`}></use>
+      </svg>
+
+      <svg width="32" height="32">
+        <use xlinkHref={`${sprite}#icon-tv`}></use>
+      </svg>
+
+      <svg width="32" height="32">
+        <use xlinkHref={`${sprite}#icon-shower`}></use>
+      </svg>
+
+      <svg width="40" height="28">
+        <use xlinkHref={`${sprite}#icon-camperVan`}></use>
+      </svg>
+
+      <svg width="40" height="28">
+        <use xlinkHref={`${sprite}#icon-camperFullyIntegrated`}></use>
+      </svg>
+
+      <svg width="40" height="28">
+        <use xlinkHref={`${sprite}#icon-camperAlcove`}></use>
+      </svg>
+
+      <svg width="24" height="22">
+        <use xlinkHref={`${sprite}#icon-heard`}></use>
+      </svg>
+
+      <svg width="24" height="22">
+        <use xlinkHref={`${sprite}#icon-heardred`}></use>
+      </svg>
+
+      <svg width="18" height="20">
+        <use xlinkHref={`${sprite}#icon-location`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-conditioner`}></use>
+      </svg>
+
+      <svg width="18" height="18">
+        <use xlinkHref={`${sprite}#icon-cd`}></use>
+      </svg>
+
+      <svg width="18" height="18">
+        <use xlinkHref={`${sprite}#icon-radio`}></use>
+      </svg>
+
+      <svg width="20" height="15">
+        <use xlinkHref={`${sprite}#icon-hob`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-toilet`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-freezer`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-gas`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-water`}></use>
+      </svg>
+
+      <svg width="20" height="20">
+        <use xlinkHref={`${sprite}#icon-microwave`}></use>
+      </svg>
+
       <h1>Home Page</h1>
     </div>
   );
